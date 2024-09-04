8a150c349440d25b207c224e3f99426c522d72d6
Jean-Baptiste LARRIVIERE <76693792+jblarriviere@users.noreply.github.com>
2024-09-04 15:41:13 +0200

    add crashing/working cases

--- App.tsx
+++ App.tsx
@@ -1,49 +1,74 @@
 import { Dimensions, View } from "react-native";
 import Animated, {
   runOnJS,
+  useAnimatedRef,
   useAnimatedScrollHandler,
+  useSharedValue,
 } from "react-native-reanimated";
-import React from "react";
-
-const workingOnBeginDrag = () => {
-  "worklet";
-  console.log("onBeginDrag");
-};
-
-const crashingOnBeginDrag = () => {
-  "worklet";
-  runOnJS(console.log)("onBeginDrag");
-};
-
-const logDragBegin = () => console.log("logDragBegin");
-
-const workingOnBeginDrag2 = () => {
-  "worklet";
-  runOnJS(logDragBegin)();
-};
-
-const crashingOnBeginDrag2 = () => {
-  "worklet";
-  logDragBegin();
-};
+import React, { useCallback } from "react";
 
 const App = () => {
-  const { width: scrollViewItemWidth, height: scrollViewItemHeight } =
-    Dimensions.get("screen");
+  const { width: scrollViewItemWidth } = Dimensions.get("screen");
+  const scrollViewRef = useAnimatedRef<Animated.ScrollView>();
+  const intervalId = useSharedValue<ReturnType<typeof setInterval> | undefined>(
+    undefined
+  );
+
+  const startInterval = useCallback(() => {
+    intervalId.value = setInterval(() => {
+      //scroll to next view or first view if we're on the last one, not relevant here
+      return null;
+    }, 7000);
+  }, [intervalId]);
+
+  const removeInterval = useCallback(() => {
+    clearInterval(intervalId.value);
+  }, [intervalId]);
 
   const handler = useAnimatedScrollHandler({
-    onBeginDrag: crashingOnBeginDrag2,
+    onBeginDrag: () => {
+      "worklet";
+
+      // This crashes with new arch enabled but works when disabled
+      runOnJS(clearInterval)(intervalId.value);
+
+      //This seems to work on both new and old arch
+      runOnJS(removeInterval)();
+    },
+    onEndDrag: () => {
+      "worklet";
+
+      runOnJS(startInterval)();
+    },
   });
 
   return (
-    <Animated.ScrollView onScroll={handler}>
-      <View
-        style={{
-          width: scrollViewItemWidth,
-          height: scrollViewItemHeight,
-          backgroundColor: "blue",
-        }}
-      />
+    <Animated.ScrollView
+      ref={scrollViewRef}
+      onScroll={handler}
+      horizontal
+      pagingEnabled
+    >
+      <View style={{ flexDirection: "row" }}>
+        <View
+          style={{
+            width: scrollViewItemWidth,
+            backgroundColor: "red",
+          }}
+        />
+        <View
+          style={{
+            width: scrollViewItemWidth,
+            backgroundColor: "green",
+          }}
+        />
+        <View
+          style={{
+            width: scrollViewItemWidth,
+            backgroundColor: "blue",
+          }}
+        />
+      </View>
     </Animated.ScrollView>
   );
 };
